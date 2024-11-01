1685cc34f6c346719c0bf99e7da3a76f7dcc02a1
towelie03 <dbalsara2003@gmail.com>
2024-11-01 09:39:15 -0700

    done

--- port_scanner.py
+++ port_scanner.py
@@ -5,15 +5,15 @@ import argparse
 import time
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Basic port scanner that mimics the behaviour of hping3.")
-    parser.add_argument('-i', '--ip', type=ip_address, required=True, help="Target IP address to capture on")
-    parser.add_argument('-p', '--port', type=int, required=True, help="Port to scan on")
+    parser = argparse.ArgumentParser(description="Basic port scanner that mimics the behaviour of hping3. If no start and end port is specified then it defaults the scan from port 1-65535")
+    parser.add_argument('ip', type=ip_address, help="Target IP address to capture on")
+    parser.add_argument('-s', '--start', type=int, default=1, help="Start port number (default is 1)")
+    parser.add_argument('-e', '--end', type=int, default=65535, help="End port number (default is 65535)")
     parser.add_argument('-d', '--delay', type=int, default=0, help="Delay in milliseconds between each scan")
 
     return parser.parse_args()
 
 def port_scan(target_ip, target_port):
-
     target_ip_str = str(target_ip)
     resp = sr1(IP(dst=target_ip_str)/TCP(dport=target_port, flags="S"), verbose=False, timeout=1)
 
@@ -21,34 +21,34 @@ def port_scan(target_ip, target_port):
         return "filtered"
 
     if TCP in resp:
-        if resp[TCP].flags == 18:
+        if resp[TCP].flags == 18:  # SYN-ACK
             return "open"
-        elif resp[TCP].flags == 4:
+        elif resp[TCP].flags == 4:  # RST
             return "closed"
-
+    
     if ICMP in resp:
-        if resp[ICMP].type == 3:
+        if resp[ICMP].type == 3:  # Destination Unreachable
             return "filtered"
 
-    return "unknown"
+    return "filtered" 
 
 def main():
     args = parse_args()
-    if args.delay > 0:
-        time.sleep(args.delay / 1000.0)  # Convert milliseconds to seconds
 
-    scan = port_scan(args.ip, args.port)
+    ports_to_scan = range(args.start, args.end + 1)
 
-    if scan == "open":
-        print(f"Port {args.port} on {args.ip} is open")
-    elif scan == "closed":
-        print(f"Port {args.port} on {args.ip} is closed")
-    elif scan == "filtered":
-        print(f"Port {args.port} on {args.ip} is filtered")
-    else:
-        print(f"Port {args.port} on {args.ip} status is unknown")
+    for port in ports_to_scan:
+        if args.delay > 0:
+            time.sleep(args.delay / 1000.0)  # Convert milliseconds to seconds
+
+        scan = port_scan(args.ip, port)
+
+        if scan == "open":
+            print(f"Port {port} on {args.ip} is open")
+        elif scan == "closed":
+            print(f"Port {port} on {args.ip} is closed")
+        elif scan == "filtered":
+            print(f"Port {port} on {args.ip} is filtered")
 
 if __name__ == "__main__":
     main()
-
-
